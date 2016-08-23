17bb3c86a43d97e6b6484a85d5bc324a6fafd6e7
Berend Wouters <berend.wouters@ugentec.com>
2016-08-23 22:38:47 +0200

    Adds function to calculate age

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -12,9 +12,21 @@ namespace FirstConsoleApplication
         {
             var name = AskQuestion("What is your name?");
             Console.WriteLine($"Hello world: {name}");
-            var age = AskQuestion("What is your age?");
+            //var age = AskQuestion("What is your age?");
+            var dateOfBirthString = AskQuestion("When were you born?");
+            var age = CalculateAge(dateOfBirthString);
             Console.WriteLine($"Your age: {age}");
             Console.ReadLine();
+        }
+
+        private static int CalculateAge(string dateOfBirthString)
+        {
+            var dateOfBirth = DateTime.Parse(dateOfBirthString);
+            var dateNow = DateTime.Now;
+            int age = dateNow.Year - dateOfBirth.Year;
+            if (dateOfBirth > dateNow.AddYears(-age))
+                age--;
+            return age;
         }
 
         private static string AskQuestion(string question)
