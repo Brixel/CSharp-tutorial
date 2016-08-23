a9bf7b0d370ebb4538deaf709f261ac77bb31ac6
Berend Wouters <berend.wouters@ugentec.com>
2016-08-23 22:17:44 +0200

    Adds debugger line to explain the debugger in VS2015

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -11,6 +11,7 @@ namespace FirstConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Debugger");
             Console.ReadLine();
         }
     }
